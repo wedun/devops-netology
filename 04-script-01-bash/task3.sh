b5ca9ed6ebcf5408b32c9d22f0e5c1db49b994d4
Dmitriy Laykom <laykomdn@yandex.ru>
2022-01-16 16:14:19 +0700

    fix bash

--- 04-script-01-bash/task3.sh
+++ 04-script-01-bash/task3.sh
@@ -4,27 +4,14 @@ ips=("192.168.0.1" "173.194.222.113" "87.250.250.242")
 error=0
 while (($error == 0))
 do
-	curl --connect-timeout 3 http://${ips[0]}:80
+    for i in "${ips[@]}"
+    do
+	curl --connect-timeout 3 http://$i:80
 	error=$?
 	if (($error != 0))
         then
 		echo ${ips[0]} >> error
 		break
         fi
-
-	curl --connect-timeout 3 http://${ips[1]}:80
-	error=$?
-	if (($error != 0))
-	then
-	        echo ${ips[1]} >> error
-		break
-	fi
-	
-	curl --connect-timeout 3 http://${ips[2]}:80
-	error=$?
-	if (($error != 0))
-	then
-	        echo ${ips[2]} >> error
-		break
-	fi
+    done
 done
