3d294c72b9b5df573739b32d1801efa14ac3779e
Dmitriy Laykom <laykomdn@yandex.ru>
2022-07-05 23:28:05 +0700

    finish 05-05

--- 05-virt-05-docker-swarm/src/terraform/variables.tf
+++ 05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -1,17 +1,17 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1gfmggr757ala32rq97"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1g79ptugpleeoj44c6k"
 }
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd884f65mo7hpqd0suh4"
+  default = "fd80g3d9gmlfodl46ujv"
 }
